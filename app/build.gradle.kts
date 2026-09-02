10126ee56db0113918ffb4e05aa190e58e333bd3
PRABAL MISHRA <prabalmishra814@gmail.com>
2026-09-02 22:12:18 +0530

    new animation/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.roomchatapps.Pmishra"
+        applicationId = "com.roomchatapps.amstudio"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
