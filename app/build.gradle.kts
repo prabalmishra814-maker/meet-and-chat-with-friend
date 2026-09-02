53eb962bf8b07ef0d33682d0da24570ca093e192
PRABAL MISHRA <prabalmishra814@gmail.com>
2026-09-02 21:48:07 +0530

    new animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.roomchatapps.amstudio"
+    namespace = "com.roomchatapps.Pmishra"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.roomchatapps.amstudio"
+        applicationId = "com.roomchatapps.Pmishra"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,5 +24,5 @@ dependencyResolutionManagement {
         maven { url = uri("https://maven.zego.im") }
     }
 }
-rootProject.name = "VOXORA"
+rootProject.name = "Room Chat"
 include(":app")
